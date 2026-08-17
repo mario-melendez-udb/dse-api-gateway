d7379e64e1bb4a1453c73c52adf42e73b6519d8c
Mario Melendez <mario.melendez@udb.edu.sv>
2026-08-16 19:00:13 -0600

    ocelot config

---
+++ LibrosAPI/Models/Libro.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibrosAPI.Models;
+
+public class Libro
+{
+    public int Id { get; set; }
+
+    [Required]
+    public string Titulo { get; set; } = string.Empty;
+
+    public string? Autor{ get; set; }
+    
+    public int AnioPublicacion { get; set; }
+}
---
+++ LibrosAPI/Models/LibrosDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LibrosAPI.Models;
+
+public class LibrosDbContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<Libro> Libros { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Libro>().HasData(
+            new Libro
+            {
+                Id = 1,
+                Titulo = "Cien años de soledad",
+                Autor = "Gabriel García Márquez ",
+                AnioPublicacion = 1967
+            },
+            new Libro{
+                Id = 2,
+                Titulo = "Don Quijote de la Mancha",
+                Autor = "Miguel de Cervantes",
+                AnioPublicacion = 1605
+            },
+            new Libro {
+                Id = 3,
+                Titulo = "El amor en los tiempos del cólera",
+                Autor = "Gabriel García Márquez ",
+                AnioPublicacion = 1985
+            }
+        );
+    }
+}
